79a62eca2b729f6e542d3f90b067375074c047ee
Barrin Chita <BarrinChita@gmail.com>
2025-07-18 09:04:52 +0100

    Innitial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import dotenv from 'dotenv';
+
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 6000;
+
+
+app.listen(PORT, ()=>console.log("App running on port: ", PORT));
