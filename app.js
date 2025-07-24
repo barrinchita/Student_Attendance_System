7bc63fab578f095271dbdae814effea1ae02616e
Barrin Chita <BarrinChita@gmail.com>
2025-07-24 02:30:56 +0100

    registration is now possible for both students and admins

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 import express from 'express'
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+import authv1Routes from './routes/authv1Routes.js';
 
 const app = express();
+app.use(express.json());
 
 dotenv.config();
 const PORT = process.env.PORT || 6000;
 
+// mongoose connection
+
+mongoose
+  .connect("mongodb://localhost/attendanceSystem")
+  .then(() => {
+    console.log("connection created");
+  })
+  .catch((e) => {
+    console.log(`Error connecting to db, Error: ${e}`);
+    res.status(500).json({failure: "Data base error"})
+  });
+
+  app.get("/", (req, res)=>{console.log("welcome")});
+
+  app.use("/api/authv1", authv1Routes);
 
 app.listen(PORT, ()=>console.log("App running on port: ", PORT));
